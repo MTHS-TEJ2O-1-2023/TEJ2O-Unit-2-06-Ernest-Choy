fe780c3ff050910342b2e03cf9ac450195c7b27a
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-09-29 13:08:48 -0400

    FIXED

--- main.ts
+++ main.ts
@@ -4,18 +4,15 @@
  * Created on: Sep 2023
  * This program Turns LED on and off
 */
-
 // On start
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 pins.digitalWritePin(DigitalPin.P16, 0)
-
 // Turn on LED
 input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.Yes)
   pins.digitalWritePin(DigitalPin.P16, 1)
 })
-
 // Turn off LED 
 input.onButtonPressed(Button.B, function () {
   basic.showIcon(IconNames.No)
